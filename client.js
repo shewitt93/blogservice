8da1cbb54d4f51d2f9e76f63c5d9894da81657ca
Despoina <despoinasifouna@gmail.com>
2020-07-26 17:32:09 +0100

    add submit form elements

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form')
+const btn = document.getElementById('submitButton')
 // const url = http://localhost:3000/
 
 
@@ -7,26 +7,24 @@ const form = document.getElementById('form')
 //   location.pathname = "/index.html"
 // }
 
-form.addEventListener('submit', submitBlog)
+btn.addEventListener('click', submitBlog)
 
 
 function submitBlog(e){
   e.preventDefault();
 
   const parseData = {
-    //add locations and data here
-    title,
-    text,
-    filmordig,
-    cameratype, 
-    lenstype,
-    image
-
+    title: e.target.title.value,
+    text: e.target.caption.value,
+    filmordig: e.target.style.value,
+    cameratype: e.target.camera.value,
+    lenstype: e.target.lens.value,
+    image: e.target.userfile.value //returns a string of a path e.g."C:\fakepath\Blossom.gif"
   }
+
   const options = {
     method: "POST",
     body: JSON.stringify(parseData)
-
   }
 
   fetch(`http://localhost:3000`, options)
@@ -45,21 +43,19 @@ function displayData () {
   const results = document.querySelector(".results")
 
   // parseData.map((e , index)=> {console.log(e)})
-  
+
   //create element
   const element = document.createElement("section")
-  
-  //append element
-  results.append(element)
+
   element.setAttribute("id", idname)
-  
+
 
   // element.setAttribute("id", index+1)
   element.setAttribute("class", "textOverImage item-b")
-  element.setAttribute("data-title", e.name)
-  element.setAttribute("data-text", e.description)
-  element.setAttribute("style", `background-image:url(${e.image})`)
+  element.setAttribute("data-title", parseData.title)
+  element.setAttribute("data-text", parseData.text)
+  element.setAttribute("style", `background-image:url(${parseData.image})`)
+
+  //append element
+  results.append(element)
 }
-
-
-
