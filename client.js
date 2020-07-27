34cf85245f340c182f9ba0a81cda02c8c45ec69c
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-07-27 15:11:14 +0100

    add Hannahs displayData function and get it displaying the blog post

--- client.js
+++ client.js
@@ -16,7 +16,6 @@ function getAllBlogs(){
     fetch('http://localhost:3000/blog')
         .then(r => r.json())
         // .then(appendBlogs) // need to append blogs to site
-        // .then(console.log)
         .catch(console.warn)
 };
 
@@ -40,11 +39,9 @@ function submitBlog(e){
 
   fetch('http://localhost:3000/blog', options)
   .then(r => r.json())
-  // .then(() => displayData(parseData))
-  .then(console.log)
+  .then(() => displayData(parseData))
   .catch(console.warn)
   }
-
 
 
 // function displayData () {
@@ -69,3 +66,46 @@ function submitBlog(e){
   // //append element
   // results.append(element)
 //
+
+
+function displayData (data) {
+  const results = document.querySelector(".results")
+  // create card
+  const card = document.createElement("div")
+  card.setAttribute("class", "card")
+    // append inner div for card image
+    const cardImage = document.createElement("div")
+    cardImage.setAttribute("class", "card-image")
+    cardImage.textContent = data.image;
+    card.appendChild(cardImage)
+    // append inner div for title
+    const cardTitle = document.createElement("p")
+    cardTitle.setAttribute("class", "title is-4")
+    cardTitle.textContent = data.title;
+    card.appendChild(cardTitle)
+    // append inner div for metadata
+    const metadataContainer = document.createElement("div")
+    metadataContainer.setAttribute("class", "card-footer")
+    card.appendChild(metadataContainer)
+    // append inner div for camera mode
+    const cameraMode = document.createElement("p")
+    cameraMode.setAttribute("class", "card-footer-item")
+    cameraMode.textContent = data.type;
+    metadataContainer.appendChild(cameraMode)
+    // append inner div for camera make
+    const cameraType = document.createElement("p")
+    cameraType.setAttribute("class", "card-footer-item")
+    cameraType.textContent = data.cameraType;
+    metadataContainer.appendChild(cameraType)
+    // append inner div for lens
+    const lens = document.createElement("p")
+    lens.setAttribute("class", "card-footer-item")
+    lens.textContent = data.lenstype;
+    metadataContainer.appendChild(lens)
+    // append inner div for text body
+    const caption = document.createElement("div")
+    caption.setAttribute("class", "content")
+    caption.textContent = data.text;
+    card.appendChild(caption)
+    results.append(card);
+}
