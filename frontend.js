b1b5c9127624e7d4e7e56a16d3b7f3e92517ab51
hannahtesfay <hannahltesfay@gmail.com>
2020-07-28 00:50:55 +0100

    fix cancel button functionality

--- frontend.js
+++ frontend.js
@@ -9,14 +9,8 @@ imageModalCloseBtn.addEventListener('click', function(e){
   modalDlg.classList.remove('is-active');
 });
 
-<<<<<<< HEAD
 var cancelButton = document.querySelector('.close-modal');
-cancelButton.addEventListener('click', function (){
-  modalDlg.classList.remove('is-active');
-});
-=======
-imageModalCloseBtn.addEventListener('click', function(e){
+cancelButton.addEventListener('click', function(e){
   e.preventDefault();
   modalDlg.classList.remove('is-active');
 });
->>>>>>> b9dba85518fcf041459a3fd9eb079579de49f957
