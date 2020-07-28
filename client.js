f5bb478c0a9a6ec89ee7593de8d03beac77b4aed
Despoina <despoinasifouna@gmail.com>
2020-07-28 11:59:23 +0100

    comment section

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const form = document.getElementById('blogForm')
 const submit = document.getElementById('submitButton');
 const image = document.querySelector("#userfile")
 const search = document.getElementById("search");
+
 // const url = http://localhost:3000/
 
 // const url = http://localhost:3000/
@@ -70,6 +71,7 @@ function submitBlog(e){
   fetch('http://localhost:3000/blog', options)
   .then(r => r.json())
   .then(() => displayData(parseData))
+  .then(() => addEvent())
   .catch(console.warn)
   }
 
@@ -109,16 +111,69 @@ function displayData (data) {
           lens.setAttribute("class", "card-footer-item")
           lens.textContent = data.lenstype;
           metadataContainer.appendChild(lens)
-      // append inner div for text body
-      const caption = document.createElement("div")
-      caption.setAttribute("class", "content")
-      caption.textContent = data.text;
-      card.appendChild(caption)
+          // append inner div for text body
+          const caption = document.createElement("div")
+          caption.setAttribute("class", "content")
+          caption.textContent = data.text;
+          card.appendChild(caption)
+          // append comment bar label
+          const commentLabel = document.createElement("label")
+          commentLabel.setAttribute("for", "commentText")
+          commentLabel.textContent = "Leave a comment: "
+          card.appendChild(commentLabel)
+          //append comment textbar
+          const commentText = document.createElement("textarea")
+          commentText.setAttribute("id", "commentText")
+          commentText.setAttribute("maxlength", "200")
+          card.appendChild(commentText)
+          //append leave comment button
+          const commentButton = document.createElement("button")
+          commentButton.setAttribute("class", "button is-success")
+          commentButton.setAttribute("id", "commentButton")
+          commentButton.textContent = "Comment"
+          card.appendChild(commentButton)
+          //append leave comment section
+          const commentSec = document.createElement("div")
+          commentSec.setAttribute("id", "commentSec")
+          card.appendChild(commentSec)
 
     results.append(card);
+
 }
 
 async function generatebase64() {
     const base64 = await image.files[0].convertToBase64()
       base64img = base64.result
   }
+
+function addEvent () {
+  const comment = document.getElementById("commentButton")
+  comment.addEventListener("click", submitComment)
+}
+
+
+function submitComment(e){
+  e.preventDefault();
+  const commentInput = document.getElementById("commentText")
+  const parseData = {
+    comment: commentInput.value
+  };
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(parseData)
+  };
+  console.log(options);
+
+  fetch('http://localhost:3000/blog', options)
+  .then(r => r.json())
+  .then(() => displayComment(parseData))
+  .catch(console.warn)
+}
+
+function displayComment (data) {
+  const section = document.querySelector("#commentSec")
+  const addComment = document.createElement("p")
+  addComment.textContent = data.comment
+  section.append(addComment)
+}
